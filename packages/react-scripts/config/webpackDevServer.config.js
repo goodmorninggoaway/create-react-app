b0a684318f70e2056c702cecf92fe564ef1d68e5
Marque Davis <marque.davis@hpe.com>
2018-10-08 12:21:06 -0400

    Serve dev assets from memory and enable livereload in shell

--- packages/react-scripts/config/webpackDevServer.config.js
+++ packages/react-scripts/config/webpackDevServer.config.js
@@ -99,5 +99,9 @@ module.exports = function(proxy, allowedHost) {
       // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
       app.use(noopServiceWorkerMiddleware());
     },
+    headers: {
+      // CORS Support
+      'Access-Control-Allow-Origin': '*',
+    },
   };
 };
