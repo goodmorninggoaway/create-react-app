b258e56a27238bf860a01eade81b02dd773519ee
Marque Davis <marque.davis@hpe.com>
2018-10-23 11:25:14 -0400

    Add microapp package name to log message

--- packages/create-react-app/createReactApp.js
+++ packages/create-react-app/createReactApp.js
@@ -191,7 +191,7 @@ function createApp(name, verbose, version, useNpm, template) {
     process.exit(1);
   }
 
-  console.log(`Creating a new React app in ${chalk.green(root)}.`);
+  console.log(`Creating a new Microapp @infosight/${appName} in ${chalk.green(root)}.`);
   console.log();
 
   const packageJson = {
