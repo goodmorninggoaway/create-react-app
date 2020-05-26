fbaf9f4b62ad9c5fdbb16544de925735f60205fb
Kevin Tronkowski <kevin.tronkowski@hpe.com>
2020-05-26 09:30:10 -0400

    Back to original headline

--- packages/cra-template-microapp/template/src/clusters/collection/components/cards/ClusterCardHeadline.js
+++ packages/cra-template-microapp/template/src/clusters/collection/components/cards/ClusterCardHeadline.js
@@ -1,20 +1,41 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import classnames from 'classnames/bind';
-import { ObjectDetailLink } from '@infosight/elmer/dist/infrastructure';
-import { OBJECTS } from '../../../../inventory/constants';
+import {ObjectDetailLink} from '@infosight/elmer/dist/infrastructure';
+import {OBJECTS} from '../../../../inventory/constants';
 import style from './style.module.scss';
 
 const cx = classnames.bind(style);
 
-function ClusterCardHeadline({ clusterInfo }) {
+function ClusterCardHeadline({clusterInfo}) {
   return (
     <div>
       <div className="row">
         <div className="small-12 large-12">
           <span className={cx('cluster-name')}>
-            <ObjectDetailLink name={clusterInfo.name} uuid={clusterInfo.uuid} objectType={OBJECTS.CLUSTER} />
-          </span>
+            <ObjectDetailLink
+              name={clusterInfo.clusterName}
+              id={clusterInfo.clusterUid}
+              objectType={OBJECTS.CLUSTER}
+            />
+                    </span>
+        </div>
+        <div className={cx('small-12', 'large-12', {invisible: clusterInfo.overutilizedCount < 0})}>
+          <span className={cx('overutilized-num', overutilizedColor)}>{clusterInfo.overutilizedCount}</span>
+        </div>
+      </div>
+      <div className="row">
+        <div className="small-12 large-12">
+          <ObjectCollectionLink objectType={OBJECTS.HOST} scope={{type: OBJECTS.CLUSTER, id: clusterInfo.clusterUid.toString()}}>
+            {clusterInfo.hostCount} Hosts
+          </ObjectCollectionLink>
+          <span>&#160;|&#160;</span>
+          <ObjectCollectionLink objectType={OBJECTS.VM} scope={{type: OBJECTS.CLUSTER, id: clusterInfo.clusterUid.toString()}}>
+            {clusterInfo.vmCount} VMs
+          </ObjectCollectionLink>
+        </div>
+        <div className={cx('small-12', 'large-12', {invisible: clusterInfo.overutilizedCount < 0})}>
+          <span className={cx('overutilized', overutilizedColor)}>{overutilizedLabel}</span>
         </div>
       </div>
     </div>
